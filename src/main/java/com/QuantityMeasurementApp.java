de8f030da2f64fc0749a59accf208982e26e19a2
Akash Sharma <i.akashhhh@gmail.com>
2026-02-22 22:31:15 +0530

    [Akash] : Added a generic length class for equality

--- src/main/java/com/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp.java
@@ -2,13 +2,20 @@ package com;
 
 public class QuantityMeasurementApp {
 
-	public static void main(String[] args) {
+    public static boolean demonstrateLengthEquality(Length l1, Length l2) {
+        return l1.equals(l2);
+    }
 
-		Feet f1 = new Feet(1.0);
-		Feet f2 = new Feet(1.0);
+    public static void main(String[] args) {
 
-		System.out.println("Equal: " + f1.equals(f2));
-		
-		Inches.demonstrateInchesEquality();
-	}
+        Length length1 = new Length(1.0, Length.LengthUnit.FEET);
+        Length length2 = new Length(12.0, Length.LengthUnit.INCHES);
+
+        System.out.println("Are lengths equal? " + length1.equals(length2));
+
+        Length length3 = new Length(1.0, Length.LengthUnit.INCHES);
+        Length length4 = new Length(1.0, Length.LengthUnit.INCHES);
+
+        System.out.println("Are lengths equal? " + length3.equals(length4));
+    }
 }
